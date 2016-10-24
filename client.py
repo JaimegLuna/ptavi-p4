e2b2d025ade11a4cc332bd4452c6a3c3bb59fa55
Jaime Garcia Luna <jluna@alpha08.aulas.gsyc.urjc.es>
2016-10-24 14:26:41 +0200

    cliente 6 terminado

--- client.py
+++ client.py
@@ -3,21 +3,38 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-import sys
+
 import socket
+import sys
 
+
+def registro(mensaje, direccion):
+    palabra = "REGISTER"
+    palabra = palabra + "sip: " + direccion + "SIP/2.0\r\n\r\n" + mensaje
+    print("Enviando: " + "REGISTER")
+    return palabra
 # Constantes. Dirección IP del servidor y contenido a enviar
-
 server= sys.argv[1]
 port=int(sys.argv[2])
-line=(' '.join)(sys.argv[3:])
+
+tipo_mensaje = sys.argv[3:]
+mi_direc = sys.argv[4]
+
+line= sys.argv[3:]
+#line=(' '.join)(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((server, port))
-    print("Enviando:", line)
-    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((server, port))
 
+frase  = ' '.join(line)
+print("Enviando:" + frase)
+my_socket.send(bytes(frase, 'utf-8') + b'\r\n')
+
+my_socket.send(bytes(registro(frase,mi_direc),'utf-8') + b'\r\n')
+data= my_socket.recv(1024)
+
+print('Recibido -- ', data.decode('utf-8'))
 print("Socket terminado.")
+
--- server.py
+++ server.py
@@ -10,22 +10,41 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    
+
+    dicc={}
+
     def handle(self):
 
-        self.wfile.write(b"Hemos recibido tu peticion")
-        IP=self.client_address
+        IP=self.client_address[0]
         print("IP: " + IP)
-        PORT=self.client_address
+        PORT=self.client_address[1]
         print("PORT: " + str(PORT))
 
-        while 1:
-            line = self.rfile.read()
-            print("El cliente nos manda: " + line.decode(utf-8))
+        self.wfile.write(b"Hemos recibido tu peticion")
+        line = self.rfile.read()
+        
+        mensaje_reci = line.decode('utf-8')
+        tipo_mensaje = mensaje_reci.split(' ')[0]
+        if tipo_mensaje == "Regsiter":
+            print("El cliente nos manda: " + tipo_mensaje)
+    
+            nombre= self.get_nombre(mensaje_reci)
+            self.dicc[nombre] = IP
+            print(self.dicc)
+            self.wfile.write(b"SIP/2.0 200 ok\r\n\r\n")
             
+        while 1: 
+            line = self.rfile.read()
+            print("El cliente nos manda: " + line.decode('utf-8'))
+            mensaje_reci=line.decode('utf-8')
+            print("El cliente nos manda: " + mensaje_reci)
+           
+          
+            if not line:
+                break
 if __name__ == "__main__":
     try:
-        serv = socketserver.UDPServer(('', 6001), SIPRegisterHandler)
+        serv = socketserver.UDPServer(('',int(sys.argv[1])), SIPRegisterHandler)
         print("Lanzando servidor UDP de eco...")
         serv.serve_forever()
     except KeyboardInterrupt:
