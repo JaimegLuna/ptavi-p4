5f41f86c32d847165c23a3147810be8fbd2c0bcc
Jaime Garcia Luna <jluna@alpha50.aulas.gsyc.urjc.es>
2017-10-30 11:26:32 +0100

    terminado server.py

--- server.py
+++ server.py
@@ -21,13 +21,20 @@ class SIPRegistHandler(socketserver.DatagramRequestHandler):
 		"""
 		self.wfile.write(b"Hemos recibido tu peticion")
 		for line in self.rfile:
-			dicc[SERVER] = port
-			print("El cliente nos manda ", dicc[SERVER])
+			if data:
+				if data[3] == '0':
+					del self.dicc[data[2]]
+					self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+				elif int(data[3]) >=0:
+					self.dicc[data[2]] = self.client_add[0], data[3]
+					print(self.dicc)
+						
+			
 
 if __name__ == "__main__":
 	#Listens at localhost('') port 6001
 	#and calls the EchoHandler class to manage the request 
-	serv = socketserver.UDPserver(('', 6001), EchoHandler)
+	serv = socketserver.UDPserver(('', int(sys.argv[1])), SIPRegistHandler)
 	
 	print("Lanzando servidor UDP de eco...")
 	try:
