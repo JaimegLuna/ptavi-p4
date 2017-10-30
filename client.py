ca72b70f0a40715339f6acb1b1b4f67363883338
Jaime Garcia Luna <jluna@alpha50.aulas.gsyc.urjc.es>
2017-10-30 10:34:18 +0100

    client.py terminado

--- client.py
+++ client.py
@@ -10,14 +10,19 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.atgv[2])
-LINE = sys.argv[3]
+LINE = sys.argv[4]
+EXPIRES = sys.argv[5]
   
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+def regist():
+	DATA = " ".join(["REGISTER sip:" LINE, EXPIRES, "SIP/2.0\r\n\r\n"])
+	my_socket.send(DATA, "utf-8"))
+	
+	
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 	my_socket.connect((SERVER, PORT))
 	print("Enviando:", LINE)
-	data = my_socket.recv(1024)
-	print('Recibido -- ', data.decode('utf-8'))
-
+	regist()
+	
 print("Socket terminado.")
